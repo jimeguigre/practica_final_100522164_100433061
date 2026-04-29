3e5e80ceef83a37726ef9b0dd96ed5ab6669888b
Jimena Guillén <jimeguigre@gmail.com>
2026-04-29 20:56:36 +0200

    .

--- log_server.c
+++ log_server.c
@@ -4,13 +4,13 @@
 int *log_operacion_1_svc(struct log_data *input, struct svc_req *rqstp) {
     static int result = 0;
 
-    printf("%s\n", input->usuario); // Imprime Nombre_usuario [cite: 193]
+    printf("%s\n", input->usuario); // Imprime Nombre_usuario 
     
     if (strlen(input->fichero) > 0) {
-        // Si hay fichero, es SENDATTACH [cite: 208, 210]
+        // Si hay fichero, es SENDATTACH 
         printf("%s %s\n", input->operacion, input->fichero);
     } else {
-        printf("%s\n", input->operacion); // Imprime OPERACION [cite: 194]
+        printf("%s\n", input->operacion); // Imprime OPERACION 
     }
 
     return &result;
